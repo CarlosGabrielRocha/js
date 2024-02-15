4f6f25af380128e1e27c18a99c3057556793e933
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-02-15 16:39:11 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,18 +1,13 @@
 
-var inicio = window.document.querySelector('input#iinicio')
-var final = window.document.querySelector('input#ifim')
-var passo = window.document.querySelector('input#ipasso')
 var cont = window.document.querySelector('div#contagem')
 function contar() {
-    /*for (var num = ini;num <= fim;num = num + pas.value) {
-       cont.innerHTML = `${num.value}`
-    }
-    */
-    var num = inicio.value
-    var pas  = passo.value
-    var fim =  final.value  
+    var num = window.document.querySelector('input#iinicio').value
+    var pas = window.document.querySelector('input#ipasso').value
+    var fim = window.document.querySelector('input#ifim').value
     while (num <= fim) {
-        cont.innerHTML = `${num}`
-        num = num + pas
+        cont.innerHTML += ` ${num}`
+        num = num += pas
     }
 }
+
+
