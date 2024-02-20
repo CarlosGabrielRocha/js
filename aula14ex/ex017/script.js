74988a4d73b2ba21fab1e8412bc925f631287149
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-02-20 16:40:40 -0300

    ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function Tabuada() {
+    let num = window.document.getElementById('inum')
+    let tab = window.document.getElementById('itab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //value vai pro php
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+}
