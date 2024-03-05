a53014792aed5144b3004e414a5d1232eded7851
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-03-05 10:38:34 -0300

    --

--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -12,8 +12,10 @@ console.log(fatorial(5))
 /*
 
 5! = 5 x 4 x 3 x 2 x 1
-5! = 5 x 4!
+5! = 5 x 4! // O fatorial de 5 é a mesma coisa que 5 x o fatorial de 4.
 
-n! = n x (n - 1)!
-1! = 1
+n! = n x (n - 1)! // Podemos chegar assim a conclusão de que um fatorial de um número pode ser calculado 
+                     baseado no fatorial de outro.
+1! = 1  // A menos que seja o fatorial de 1,pois sabemos que matemáticamente o fatorial de 1 equivale a 1.  
+
 */
