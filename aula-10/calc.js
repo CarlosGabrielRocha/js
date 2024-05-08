9ea82d8ec3d8f5688c16b656e4b8733f244e16f0
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-05-08 19:50:27 -0300

    ex 01 e ex 02

---
+++ aula-10/calc.js
@@ -0,0 +1,9 @@
+var n1 = parseFloat(prompt('Digite o primeiro número:'))
+var n2 = parseFloat(prompt('Digite o Segundo número:'))
+
+var x = n1 * n2 
+var d = n1 / n2
+var s = n1 + n2
+var m = n1 - n2
+
+alert(`RESULTADO - soma: ${s}   subtração: ${m}   multiplicação: ${x}   divisão: ${d} `)
---
+++ ex01/index.js
@@ -0,0 +1,9 @@
+alert('Olá Recruta! Faremos o seu alistamento.')
+var nome = window.prompt("Insira o seu primeiro nome:")
+var snome = window.prompt("Agora seu sobrenome:")
+var camp = window.prompt("Insira o seu campo de estudo:")
+var ano = window.prompt("Por fim, digite seu ano de nascimento:")
+
+var idade = 2024 - ano
+alert(`Ótimo, seu nome é ${nome} ${snome} e você tem ${idade} anos. Campo de Estudo: ${camp}.`)
+
