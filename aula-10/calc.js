e6b1ed4a1698f5235894ee750dbb333ad15b3883
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-05-09 09:48:52 -0300

    Update calc.js

--- aula-10/calc.js
+++ aula-10/calc.js
@@ -1,9 +1,9 @@
-var n1 = parseFloat(prompt('Digite o primeiro número:'))
-var n2 = parseFloat(prompt('Digite o Segundo número:'))
+const n1 = parseFloat(prompt('Digite o primeiro número:'))
+const n2 = parseFloat(prompt('Digite o Segundo número:'))
 
-var x = n1 * n2 
-var d = n1 / n2
-var s = n1 + n2
-var m = n1 - n2
+let x = n1 * n2 
+let d = n1 / n2
+let s = n1 + n2
+let m = n1 - n2
 
 alert(`RESULTADO - soma: ${s}   subtração: ${m}   multiplicação: ${x}   divisão: ${d} `)
