4d515b1c4cf11800267ab313fd9a1c2b16c3e432
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-05-13 16:20:33 -0300

    fazendo ex 03

---
+++ ex03/index.js
@@ -0,0 +1,13 @@
+const valor = prompt('Digite o valor em METROS a ser convertido: ')
+const medida = prompt(`
+                Escolha a unidade de medida:
+                       -milímetro  (mm)
+                       -centímetro (cm)
+                       -decímetro  (dm) 
+                       -decâmetro  (dam)
+                       -hectômetro (hm)
+                       -quilômetro (km)
+                    `)
+switch (medida) {
+    
+}
