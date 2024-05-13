7f0b3c3d3fb1c8784f751c78718f4357ba245c52
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-05-13 17:31:12 -0300

    Update index.js

--- ex03/index.js
+++ ex03/index.js
@@ -1,4 +1,4 @@
-const valor = prompt('Digite o valor em METROS a ser convertido: ')
+const valor = parseFloat(prompt('Digite o valor em METROS a ser convertido: '))
 const medida = prompt(`
                 Escolha a unidade de medida:
                        -milímetro  (mm)
@@ -8,6 +8,28 @@ const medida = prompt(`
                        -hectômetro (hm)
                        -quilômetro (km)
                     `)
+let res
 switch (medida) {
-    
+   case 'mm':
+      res = valor * 1000
+   break
+   case 'cm':
+       res = valor * 100
+   break
+   case 'dm':
+      res = valor * 10
+   break
+   case 'dam':
+      res = valor / 10
+   break
+   case 'hm':
+      res = valor / 100
+   break
+   case 'km':
+      res = valor / 1000
+   break
+   default:
+      alert("[OPÇÃO INVÁLIDA]")
 }
+
+alert(`Resultado da conversão: ${res}`)
