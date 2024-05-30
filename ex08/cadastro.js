48234a8ebe84ed0eb6877dd19a9744aff2624cc7
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-05-30 13:49:37 -0300

    Update cadastro.js

--- ex08/cadastro.js
+++ ex08/cadastro.js
@@ -1,23 +1,25 @@
 let pick = ""
-let cadas = [["Proprietário(a): Dayse", " Quartos: 2", " Banheiros: 1", " Garagem: N"]]
-let mostrar = ""
+let cadas = [{proprietario: "Dayse", quartos: "2", banheiro: "1", garagem: "N"}]
+
+
 do {
    pick = prompt(`Quantidade de imóveis cadastrados: ${cadas.length}\n[DIGITE O NÚMERO DE UM DAS OPÇÕES]
 1 - Salvar imóvel\n2 - Mostras imóveis salvos\n3 - Sair `)
 
 switch(pick){
     case "1":
-        let dados = []
-        dados.push(`Proprietário(a): ${prompt(`Nome do proprietário:`)} `) 
-        dados.push(` Quartos: ${prompt(`Quantidade de quartos:`)} `) 
-        dados.push(` Banheiros: ${prompt(`Quantidade de banheiros:`)} `)
-        dados.push(` Garagem: ${prompt(`Possui garagem?[N/S]`)}`)
+        const dados = {}
+        dados.proprietario =`Proprietário(a): ${prompt(`Nome do proprietário:`)} `
+        dados.quartos = ` Quartos: ${prompt(`Quantidade de quartos:`)} ` 
+        dados.banheiro = ` Banheiros: ${prompt(`Quantidade de banheiros:`)} `
+        dados.garagem = ` Garagem: ${prompt(`Possui garagem?[N/S]`)}`
 
         cadas.push(dados)
     break
     case "2":
         for(let i = 0; i < cadas.length; i++) {
-            mostrar += (`\nImóvel ${i+1}:\n${cadas[i]}`)
+            alert(`\nImóvel ${i+1}:\n${cadas[i].proprietario}, ${cadas[i].quartos}, ${cadas[1].banheiro}, 
+${cadas[1].garagem}`)
         }
         alert(`IMOVÉIS SALVOS:\n${mostrar}`)
         mostrar = ""
