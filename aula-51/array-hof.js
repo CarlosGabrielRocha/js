f38fea7e8f2f4173db0155a986f1e40f6b817987
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-06-11 15:51:58 -0300

    aula-51

--- aula-51/array-hof.js
+++ aula-51/array-hof.js
@@ -16,7 +16,7 @@ numeros.forEach(function(numero) { //numero = elemento
 
 console.log(soma) // 15*/
 
-const letras = ["a", "b", "a", "c", "a", "b"]
+/*const letras = ["a", "b", "a", "c", "a", "b"]
 const contagem = {}
 
 letras.forEach(function(letra) {
@@ -27,4 +27,68 @@ letras.forEach(function(letra) {
     }
 })
 
-console.log(contagem) // { a: 3, b: 2, c: 1 }
+console.log(contagem) // { a: 3, b: 2, c: 1 }*/
+
+const personagens = [
+    { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+    { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+    { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+    { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+    { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+    { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+  ]
+
+  // Map
+
+  /*const nomes = []
+
+  for(let i = 0; i < personagens.length; i ++) {
+    nomes.push(personagens[i].nome)
+  }*/
+
+  const nomes = personagens.map(function(personagem){
+    return personagem.nome
+  })
+
+  console.log(nomes)
+
+  // Filter
+
+  /*const orcs = []
+
+  for(let i = 0; i < personagens.length; i++) {
+    if (personagens[i].raca === "Orc") {
+        orcs.push(personagens[i])
+    }
+  }*/
+
+  const orcs = personagens.filter(function(personagem) {
+    return personagem.raca === 'Orc'
+  })
+
+  console.log(orcs)
+
+  //Reduce
+
+  const nivelTotal = personagens.reduce(function(valorAcumulado, personagem) {
+    return valorAcumulado + personagem.nivel
+  },0)
+
+  console.log(nivelTotal)
+
+  const racas = personagens.reduce(function(valorAcumulado, personagem){
+    if(valorAcumulado[personagem.raca]) {
+        valorAcumulado[personagem.raca].push(personagem)
+    } else {
+        valorAcumulado[personagem.raca] = [personagem]
+    }
+
+    return valorAcumulado
+  }, {})
+  
+  console.log(racas)
+
+
+
+
