c093230b06b63431ff01f97242355728543a0106
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-06-27 13:24:48 -0300

    Fazendo o ex 10

--- ex10/index.js
+++ ex10/index.js
@@ -1,46 +1,78 @@
 const vagas = []
 
+//Funções de Validação
+
+function validarData(dataLimite) {
+    let dataAtual = new Date()
+    dataModificada = dataLimite.replace(/\//g,'-')
+    dataTeste = new Date(dataModificada)
+
+    let regex = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/
+    let validandoData = regex.test(dataLimite)
+    let dataValida
+
+    if (validandoData !== true) {
+        alert('[A data informada se encontra em um formato errado!]')
+        dataValida = false
+        return dataValida
+    } else if(dataTeste.getTime() < dataAtual.getTime()) {
+        alert('[A data informada é anterior à data atual.]')
+        dataValida = false
+        return dataValida
+    } else {
+        dataValida = true
+        return dataValida
+    }
+ 
+}
+
+//Funções ligadas ao menu
 
 function criarVaga() {
-    // Variáveis para teste de data
-    let results = false
-    let regex = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/
-    let dataLimite = ''
-    let tentativas = 0
 
     let nome = prompt(`Digite o nome da VAGA:`)
     let descricao = prompt(`Descrição da VAGA:`)
 
+    let dataValida = 'false'
+    let dataLimite = ''
+
     do {
         dataLimite = prompt(`Qual será a data limite para inscrição nessa VAGA? EX: dd/mm/aaaa`)
-        results = regex.test(dataLimite)
-        if (results !== true) {
-            alert('Data inválida!')
-            tentativas++
-            alert(tentativas)
-        }
-        if (tentativas > 3) {
+        dataValida = validarData(dataLimite)
+    } while (dataValida !== true)
+
+    if (dataValida) {
+        let confirmacao = confirm(`Tem certeza que deseja adicionar essa VAGA?\n---------------\nNome da Vaga: ${nome}\nDescrição:\n${descricao}\nData Limite: ${dataLimite}`)
+
+        if (confirmacao) {
+            vagas.push({
+                'nome': nome,
+                'descricao': descricao,
+                'dataLimite': dataLimite
+            })
+            alert('Vaga adicionada com sucesso!')
+        } else {
             alert('Voltando ao MENU..')
         }
-    } while (results !== true || tentativas <= 3)
-
-    let confirmacao = confirm(`Tem certeza que deseja adicionar essa VAGA?\n---------------\nNome da Vaga: ${nome}\nDescrição:\n${descricao}\nData Limite: ${dataLimite}`)
-
-    if (confirmacao) {
-        vagas.push({
-            'nome': nome,
-            'descricao': descricao,
-            'dataLimite': dataLimite
-         })
-         alert('Vaga adicionada com sucesso!')
-    } else {
-        alert('Voltando ao MENU..')
     }
+
 }
+
+// TERMINAR!!!!!!!
+
+function listarVagas() {
+    let lista = vagas.reduce(function(acumulativo, elemento) {
+        acumulativo += `Vaga(${vagas.indexOf(elemento)})\n${elemento.nome}\nNúmero de inscritos:`
+    }, '')
+    
+    alert(lista)
+}
+
+//Menu
 
 function menu() {
     let opt = prompt(`1. Vagas disponíveis\n2. Criar nova vaga\n3. Vizualizar vaga\n4. Inscrever candidato em vaga\n5. Excluir vaga\n6. Sair `)
-    if(opt === '6') {
+    if (opt === '6') {
         return alert('SAINDO..')
     } else {
         opcoes(opt)
@@ -50,9 +82,12 @@ function menu() {
 
 function opcoes(opt) {
     switch (opt) {
+        case '1':
+            listarVagas()
+            break
         case '2':
-        criarVaga()
-        break
+            criarVaga()
+            break
         default:
             alert('[OPÇÃO INVÁLIDA]')
     }
