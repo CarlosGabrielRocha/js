f9031781b83dfa0f04f05649ee2a557a2bb645b2
Carlos Gabriel <148346104+CarlosGabrielRocha@users.noreply.github.com>
2024-09-03 13:08:54 -0300

    plugins do webpack

--- modulo-3/16-usando-o-webpack/webpack.config.js
+++ modulo-3/16-usando-o-webpack/webpack.config.js
@@ -1,14 +1,18 @@
-const path = require('path')
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: {
         index: './src/index.js',
     },
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [{
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
         }]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
 }
