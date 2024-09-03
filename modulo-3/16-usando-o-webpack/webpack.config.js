1cf01c74a7752f635b954f85de7e01bd98a9a29b
Carlos Gabriel <148346104+CarlosGabrielRocha@users.noreply.github.com>
2024-09-03 11:26:28 -0300

    Loaders do webpack

--- modulo-3/16-usando-o-webpack/webpack.config.js
+++ modulo-3/16-usando-o-webpack/webpack.config.js
@@ -3,11 +3,12 @@ const path = require('path')
 module.exports = {
     entry: {
         index: './src/index.js',
-        hello: './src/hello.js'
     },
     mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].bundle.min.js'
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
     }
 }
